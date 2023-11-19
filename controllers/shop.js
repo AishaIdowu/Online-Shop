5f227545be863bb56213f6c640f228f4d87c5acf
AishaIdowu <idowueniola@domain.com>
2023-11-19 18:59:44 +0100

    Retrieving all products with sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,17 +2,20 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then((rows) => {
-      res.render("shop/product-list", {
-        prods: rows,
-        pageTitle: "All Products",
-        path: "/products",
-      });
-    })
-    .catch((err) => console.log(err));
+  Product.findAll()
+  .then(products => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  })
+  .catch(err => {
+    console.log(err)
+  })
+  
 };
-
+ 
 exports.getProduct = (req, res, next) => {
   // The name use in routes will also be used in params /:productId
   const prodId = req.params.productId;
@@ -26,15 +29,18 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render("shop/index", {
-        prods: rows,
-        pageTitle: "Shop",
-        path: "/",
-      });
-    })
-    .catch((err) => console.log(err));
+  Product.findAll()
+  .then(products => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  })
+  .catch(err => {
+    console.log(err)
+  })
+   
 };
 
 
